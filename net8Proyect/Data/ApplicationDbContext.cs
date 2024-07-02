e6842779ec4c2be09535bfd40931ae500f174a64
DavidMena18 <davidmena2500@gmail.com>
2024-07-02 11:19:47 -0400

    agregue un comentario en el ApplicationDbContext

--- net8Proyect/Data/ApplicationDbContext.cs
+++ net8Proyect/Data/ApplicationDbContext.cs
@@ -3,11 +3,15 @@ using Microsoft.EntityFrameworkCore;
 
 namespace net8Proyect.Data
 {
+    //ahora el dbContext no hereda de DbContext ahora es de IdentityDbContext
+    //Identity nos ayudara con lo que viene siendo la clasificacion de roles en el programa y con la seguridad los log in etc
     public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
+        //aqui van todos los modelos
     }
 }
