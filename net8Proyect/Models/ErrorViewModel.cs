e4319b25ec746e1fac2fc243a727107983d76b7f
DavidMena18 <davidmena2500@gmail.com>
2024-07-02 10:52:29 -0400

    Add project files.

---
+++ net8Proyect/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace net8Proyect.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
