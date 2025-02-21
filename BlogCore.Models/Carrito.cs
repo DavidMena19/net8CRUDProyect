517b306e57999bb9452b6b0333b18e3d3a7bd163
DavidMena18 <davidmena2500@gmail.com>
2025-02-21 09:40:13 -0400

    CarritoFKChanges

--- BlogCore.Models/Carrito.cs
+++ BlogCore.Models/Carrito.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,10 +13,10 @@ namespace net8Proyect.Models
         [Key]
         public int Id { get; set; }
         //cliente id en string porque el id de los usuarios en identiti es en string no int
-        public string ClienteId { get; set; }
-        public int ArticuloId { get; set; }
+        public string ClienteId { get; set; }      
+        public int? ArticuloId { get; set; }
+        [ForeignKey("ArticuloId")]
         public Articulo Articulo { get; set; }
-
         public List<CarritoDetalle> Detalles { get; set; } = new List<CarritoDetalle>();
         public Decimal Total => Detalles.Sum(d => d.Subtotal);
 
